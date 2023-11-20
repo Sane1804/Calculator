06b4e0e8518d61d51baac1866a799f7ccfcbfbff
Alexander Vielma <vielmaa1804@gmail.com>
2023-11-20 08:35:09 -0400

    finish UI

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ const SECOND_SCREEN = document.querySelector("#second-screen");
 const appendBtn = (node) => {
     let box = node;
 
-    for (let i = 0; i < 18; i++){
-        let arr = ["C", "DELETE", "7", "8", "9", "/", "4", "5" ,"6", "x", "1", "2", "3", "-", ".", "0", "=", "+"];
-        let btn = document.createElement("button");
-        btn.classList.add("btn");
-        if(i < 2){
-            btn.classList.add(`btn-span${i}`);
-        }
-        btn.textContent = arr[i];
-        box.appendChild(btn);
+    let div = document.createElement("div")
+    div.classList.add("autor")
+    div.textContent = "Made By Sane"
+    box.appendChild(div)
+
+    let arr = ["C", "DELETE", "7", "8", "9", "/", "4", "5" ,"6", "x", "1", "2", "3", "-", ".", "0", "=", "+"];
+    for (let i = 0; i < arr.length; i++){
+      let btn = document.createElement("button");
+      btn.classList.add("btn");
+      btn.textContent = arr[i]
+      box.appendChild(btn);
     }
 }
 appendBtn(BTN_CONTAINER)
